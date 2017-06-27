3c195a963ffa45cdc2147bcc2b33367ab2255f16
Maycon V. Moreira <mayconvm@gmail.com>
2017-06-27 20:58:55 -0300

    Layout atualizado.

--- project/test/resources/views/welcome.php
+++ project/test/resources/views/welcome.php
@@ -32,15 +32,6 @@
     <style type="text/css">
         
 
-        .details {
-          position: relative;
-          top: -100px;
-        }
-
-        .details .card-title {
-          padding-bottom: 20px;
-        }
-
         .details .nav-content {
           height: 216px;
         }
@@ -83,27 +74,29 @@
   <body ng-controller="TesteController" id="TesteController">
     
     <div ng-show="details">
-      <nav>
-        <div class="nav-wrapper light-blue">
-          <a href="#" class="brand-logo left">Cursos</a>
+      <div class="navbar-fixed">
+        <nav>
+          <div class="nav-wrapper light-blue">
+            <a href="#" class="brand-logo left">Cursos</a>
 
-          <div class="row">
-            
-            <a class="right" href="javascript:void(0)" ng-show="!showSearch" ng-click="showSearch = true">
-                <i class="material-icons left">search</i>
-              </a>
+            <div class="row">
+              
+              <a class="right" href="javascript:void(0)" ng-show="!showSearch" ng-click="showSearch = true">
+                  <i class="material-icons left">search</i>
+                </a>
 
-            <div class="col offset-l4 l4 s8 offset-s4">
-                <div class="input-field blue" ng-show="isDesktop || showSearch">
-                  <input id="search" type="search" class="search" required ng-model="textSearch" placeholder="Pesquisa" ng-blur="showSearch = isMobile? false : true">
-                  <label class="label-icon" for="search"><i class="material-icons">search</i></label>
-                  <i class="material-icons" ng-show="isDesktop">close</i>
-                </div>
-            </div>
-          </div>        
+              <div class="col offset-l4 l4 s8 offset-s4">
+                  <div class="input-field blue" ng-show="isDesktop || showSearch">
+                    <input id="search" type="search" class="search" required ng-model="textSearch" placeholder="Pesquisa" ng-blur="showSearch = isMobile? false : true">
+                    <label class="label-icon" for="search"><i class="material-icons">search</i></label>
+                    <i class="material-icons" ng-show="isDesktop">close</i>
+                  </div>
+              </div>
+            </div>        
 
-        </div>
-      </nav>
+          </div>
+        </nav>
+      </div>
 
         <div class="container">
 
@@ -135,63 +128,52 @@
         </div>
     </div>
 
+    <!-- DETAILS -->
+
     <div ng-show="!details">
+      
       <nav class="nav-extended">
         <div class="nav-wrapper light-blue darken-2">
-          
-
-
-          <div class="nav-content">
-            <ul class="left hide-on-med-and-down">
-              <li>
-                <a class="" href="javascript:void(0)" ng-click="details = true">
-                  <i class="material-icons left">arrow_back</i>
-                </a>
-              </li>
-            </ul>
-
-            <br> <br> <br> <br>
-            <br> <br> <br> <br>
-            <br> <br> <br> <br>
+          <ul class="left">
+            <li>
+              <a class="" href="javascript:void(0)" ng-click="details = true">
+                <i class="material-icons left">arrow_back</i>
+              </a>
+            </li>
+          </ul>
+          <br>
+          <div class="row">
+            <div class="col s10 m10">
+              <h5 class="title">{{ itemDetail.title }}</h5>
+            </div>
           </div>
-
         </div>
       </nav>
 
       <div class="container details">
 
+        <div class="grey-text">
+          <p class="">
+            {{ itemDetail.description }} 
+          </p>
 
-        <div class="row">
-          <div class="col s12 m12">
-            
-            <div class="card white-grey lighten-4">
-              
-              <div class="card-content">
-                <span class="card-title">{{ itemDetail.title }}</span>
-                <p class="text-grey">
-                  {{ itemDetail.description }}
-                </p>
-
-
-                <div class="list">
-                  <ul>
-                    <li> <i class="material-icons">today</i> {{ itemDetail.start | customDate }} </li>
-                    <li> <i class="material-icons">query_builder</i> De 14:30 as 16:00 - 1:30h </li>
-                    <li> <i class="material-icons">room</i> <a target="_new" href="http://maps.google.com"> {{ itemDetail.address.street }}, {{ itemDetail.address.number }}, {{ itemDetail.address.neighborhood }} - {{ itemDetail.address.city }} </a></li>
-                    <li> <i class="material-icons">send</i> R$ {{ itemDetail.price }} </li>
-                    <li> <i class="material-icons">label</i> {{ itemDetail.category }} </li>
-                    <li> <img src="{{ itemDetail.consultant.avatar }}" alt="" class="circle"> {{ itemDetail.consultant.name }} </li>
-                  </ul>
-                </div>
-
-                <div class="row">
-                  <div class="col m5"></div>
-                  <a class="waves-effect waves-light btn orange accent-3">INSCRIÇÃO</a>
-                </div>
-
-            </div>
+          <div class="list">
+            <ul>
+              <li> <i class="material-icons">today</i> {{ itemDetail.start | customDate }} </li>
+              <li> <i class="material-icons">query_builder</i> De 14:30 as 16:00 - 1:30h </li>
+              <li> <i class="material-icons">room</i> <a target="_new" href="http://maps.google.com"> {{ itemDetail.address.street }}, {{ itemDetail.address.number }}, {{ itemDetail.address.neighborhood }} - {{ itemDetail.address.city }} </a></li>
+              <li> <i class="material-icons">send</i> R$ {{ itemDetail.price }} </li>
+              <li> <i class="material-icons">label</i> {{ itemDetail.category }} </li>
+              <li> <img src="{{ itemDetail.consultant.avatar }}" alt="" class="circle"> {{ itemDetail.consultant.name }} </li>
+            </ul>
           </div>
 
+          <div class="row">
+            <div class="col offset-m4 m4 s4 offset-s4">
+              <a class="waves-effect waves-light btn orange accent-3 black-text">INSCRIÇÃO</a>
+            </div>
+          </div>
+          
         </div>
       </div>
     </div>
