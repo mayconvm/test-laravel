74f5705669ec2b7943b7611048cf4f61ecdabd5c
Maycon V. Moreira <mayconvm@gmail.com>
2017-06-27 08:25:11 -0300

    Adicionado os métodos~ para realizar os relacionamentos.

--- project/test/app/Course.php
+++ project/test/app/Course.php
@@ -7,4 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 class Course extends Model
 {
     protected $primaryKey = "idcourses";
+
+    protected $hidden = array('consultants_idconsultants', 'addresses_idaddresses', 'created_at', 'updated_at');
+
+    public function address()
+    {
+    	return $this->hasOne('App\Address', 'idaddresses');
+    }
+
+    public function consultant()
+    {
+    	return $this->hasOne('App\Consultant', 'idconsultants');
+    }
 }
